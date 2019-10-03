92b6f4e44e2ee2da908814cc45a2a3e1af68ca83
Michael Aerni <hello@michaelaerni.ch>
2019-10-03 12:33:14 +0200

    Release of version 2.1.0
    Added utilities for width, maxWidth, minWidth, height, maxHeight and minHeight.

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,12 @@ const tailwindcssRfs = function(userConfig = {}) {
             fontSizeUtilities: true,
             paddingUtilities: true,
             marginUtilities: true,
+            widthUtilities: false,
+            maxWidthUtilities: false,
+            minWidthUtilities: false,
+            heightUtilities: false,
+            maxHeightUtilities: false,
+            minHeightUtilities: false,
         };
 
         const options = _.defaults({}, userConfig, defaultOptions);
@@ -46,13 +52,49 @@ const tailwindcssRfs = function(userConfig = {}) {
             [`.${e(prefixNegativeModifiers('ml', modifier))}`]: { 'margin-left': `rfs(${size})` },
         });
 
+        const widthGenerator = (size, modifier) => ({
+            [`.${e(`${options.prefix}w-${modifier}${options.suffix}`)}`]: { width: `rfs(${size})` },
+        });
+
+        const maxWidthGenerator = (size, modifier) => ({
+            [`.${e(`${options.prefix}max-w-${modifier}${options.suffix}`)}`]: { 'max-width': `rfs(${size})` },
+        });
+
+        const minWidthGenerator = (size, modifier) => ({
+            [`.${e(`${options.prefix}min-w-${modifier}${options.suffix}`)}`]: { 'min-width': `rfs(${size})` },
+        });
+
+        const heightGenerator = (size, modifier) => ({
+            [`.${e(`${options.prefix}h-${modifier}${options.suffix}`)}`]: { height: `rfs(${size})` },
+        });
+
+        const maxHeightGenerator = (size, modifier) => ({
+            [`.${e(`${options.prefix}max-h-${modifier}${options.suffix}`)}`]: { 'max-height': `rfs(${size})` },
+        });
+
+        const minHeightGenerator = (size, modifier) => ({
+            [`.${e(`${options.prefix}min-h-${modifier}${options.suffix}`)}`]: { 'min-height': `rfs(${size})` },
+        });
+
         const fontSizeUtilities = _.flatMap(theme('fontSize'), fontSizeGenerator);
         const paddingUtilities = _.flatMap(theme('padding'), paddingGenerator);
         const marginUtilities = _.flatMap(theme('margin'), marginGenerator);
+        const widthUtilities = _.flatMap(theme('width'), widthGenerator);
+        const maxWidthUtilities = _.flatMap(theme('maxWidth'), maxWidthGenerator);
+        const minWidthUtilities = _.flatMap(theme('minWidth'), minWidthGenerator);
+        const heightUtilities = _.flatMap(theme('height'), heightGenerator);
+        const maxHeightUtilities = _.flatMap(theme('maxHeight'), maxHeightGenerator);
+        const minHeightUtilities = _.flatMap(theme('minHeight'), minHeightGenerator);
 
         if (options.fontSizeUtilities) addUtilities(fontSizeUtilities, variants('rfsFontSize'));
         if (options.paddingUtilities) addUtilities(paddingUtilities, variants('rfsPadding'));
         if (options.marginUtilities) addUtilities(marginUtilities, variants('rfsMargin'));
+        if (options.widthUtilities) addUtilities(widthUtilities, variants('rfsWidth'));
+        if (options.maxWidthUtilities) addUtilities(maxWidthUtilities, variants('rfsMaxWidth'));
+        if (options.minWidthUtilities) addUtilities(minWidthUtilities, variants('rfsMinWidth'));
+        if (options.heightUtilities) addUtilities(heightUtilities, variants('rfsHeight'));
+        if (options.maxHeightUtilities) addUtilities(maxHeightUtilities, variants('rfsMaxHeight'));
+        if (options.minHeightUtilities) addUtilities(minHeightUtilities, variants('rfsMinHeight'));
     };
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
         rfsFontSize: [], // default: []
         rfsPadding: [], // default: []
         rfsMargin: [], // default: []
+        rfsWidth: [], // default: []
+        rfsMaxWidth: [], // default: []
+        rfsMinWidth: [], // default: []
+        rfsHeight: [], // default: []
+        rfsMaxHeight: [], // default: []
+        rfsMinHeight: [], // default: []
     },
     plugins: [
         require('./dist/index')({
@@ -11,11 +17,23 @@ module.exports = {
             fontSizeUtilities: true, // default: true
             paddingUtilities: true, // default: true
             marginUtilities: true, // default: true
+            widthUtilities: false, // default: false
+            maxWidthUtilities: false, // default: false
+            minWidthUtilities: false, // default: false
+            heightUtilities: false, // default: false
+            maxHeightUtilities: false, // default: false
+            minHeightUtilities: false, // default: false
         }),
     ],
     corePlugins: {
         fontSize: false,
         padding: false,
         margin: false,
+        width: true,
+        maxWidth: true,
+        minWidth: true,
+        height: true,
+        maxHeight: true,
+        minHeight: true,
     },
 };
