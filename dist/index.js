eb5c92380065b8e6c6b42e2e6dec8ef259d30fa8
Michael Aerni <hello@michaelaerni.ch>
2020-12-21 14:25:37 +0100

    Properly escape class name

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin(function ({ addVariant }) {
+module.exports = plugin(function ({ addVariant, e }) {
     addVariant('rfs', ({ container, separator }) => {
         container.walkRules(rule => {
-            rule.selector = `.\\rfs${separator}${rule.selector.slice(1)}`
+            rule.selector = `.${e(`rfs${separator}${rule.selector.slice(1)}`)}`
             rule.walkDecls(decl => {
                 decl.value = `rfs(${decl.value})`
             })
