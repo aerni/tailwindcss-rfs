50e6058ea83d16f90774171bc153b092c55b4bd1
Michael Aerni <hello@michaelaerni.ch>
2020-12-21 12:02:22 +0100

    Refactor to variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,40 +5,22 @@ module.exports = {
         extend: {},
     },
     variants: {
-        rfsFontSize: [], // default: []
-        rfsPadding: [], // default: []
-        rfsMargin: [], // default: []
-        rfsWidth: [], // default: []
-        rfsMaxWidth: [], // default: []
-        rfsMinWidth: [], // default: []
-        rfsHeight: [], // default: []
-        rfsMaxHeight: [], // default: []
-        rfsMinHeight: [], // default: []
+        extend: {
+            fontSize: ['rfs'],
+            lineHeight: ['rfs'],
+            space: ['rfs'],
+            padding: ['rfs'],
+            margin: ['rfs'],
+            width: ['rfs'],
+            minWidth: ['rfs'],
+            maxWidth: ['rfs'],
+            height: ['rfs'],
+            minHeight: ['rfs'],
+            maxHeight: ['rfs'],
+            gap: ['rfs'],
+            inset: ['rfs'],
+            translate: ['rfs'],
+        },
     },
-    plugins: [
-        require('./dist/index')({
-            prefix: 'rfs-', // default: 'rfs-'
-            suffix: '', // default: ''
-            fontSizeUtilities: true, // default: true
-            paddingUtilities: true, // default: true
-            marginUtilities: true, // default: true
-            widthUtilities: false, // default: false
-            maxWidthUtilities: false, // default: false
-            minWidthUtilities: false, // default: false
-            heightUtilities: false, // default: false
-            maxHeightUtilities: false, // default: false
-            minHeightUtilities: false, // default: false
-        }),
-    ],
-    corePlugins: {
-        fontSize: false,
-        padding: false,
-        margin: false,
-        width: true,
-        maxWidth: true,
-        minWidth: true,
-        height: true,
-        maxHeight: true,
-        minHeight: true,
-    },
+    plugins: [require('./dist/index')],
 }
