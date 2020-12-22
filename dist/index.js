e70eef84d02810f22c901cd1a9ab0a764c7e9948
Michael Aerni <hello@michaelaerni.ch>
2020-12-22 12:51:34 +0100

    Add special handling for space between utilities

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,38 @@
 const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin(function ({ addVariant, e }) {
+module.exports = plugin(function ({ addVariant }) {
     addVariant('rfs', ({ container, separator }) => {
         container.walkRules(rule => {
-            rule.selector = `.${e(`rfs${separator}${rule.selector.slice(1)}`)}`
-            rule.walkDecls(decl => {
-                decl.value = `rfs(${decl.value})`
-            })
+            rule.selector = rule.selector.slice(1)
+
+            if (isGeneralUtility(rule.selector)) {
+                handleGeneralUtilities(rule)
+            } else {
+                handleSpaceBetweenUtilities(rule)
+            }
+
+            rule.selector = `.rfs\\${separator}${rule.selector}`
         })
     })
+
+    const isGeneralUtility = function (selector) {
+        if (/^-?space-(x|y)-/.test(selector)) return false
+
+        return true
+    }
+
+    const handleGeneralUtilities = function (rule) {
+        rule.walkDecls(decl => {
+            decl.value = `rfs(${decl.value})`
+        })
+    }
+
+    const handleSpaceBetweenUtilities = function (rule) {
+        rule.walkDecls(decl => {
+            let rawValue = decl.value.replace('calc(', '').split(' ')[0]
+            let rfs = `rfs(${rawValue})`
+
+            decl.value = decl.value.replace(rawValue, rfs)
+        })
+    }
 })
