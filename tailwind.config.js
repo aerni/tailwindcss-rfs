cbd9a2ac5ac3789f136d100e6d5396b900b8af98
Michael Aerni <hello@michaelaerni.ch>
2022-03-15 15:24:09 -0700

    Update Tailwind CSS to v3.0.23

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,4 @@
 module.exports = {
-    purge: [],
-    darkMode: false,
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {
-            fontSize: ['rfs'],
-            lineHeight: ['rfs'],
-            padding: ['rfs'],
-            margin: ['rfs'],
-            gap: ['rfs'],
-            space: ['rfs'],
-            width: ['rfs'],
-            minWidth: ['rfs'],
-            maxWidth: ['rfs'],
-            height: ['rfs'],
-            minHeight: ['rfs'],
-            maxHeight: ['rfs'],
-            borderRadius: ['rfs'],
-            inset: ['rfs'],
-            translate: ['rfs'],
-        },
-    },
+    content: ['example/index.html'],
     plugins: [require('./dist/index')],
 }
